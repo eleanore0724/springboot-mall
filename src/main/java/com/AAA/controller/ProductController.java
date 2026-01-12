f029c8585bd6a1f65819a6f3a4bea99403f37f64
eleanore0724 <65076389+eleanore0724@users.noreply.github.com>
2026-01-13 01:34:03 +0800

    add update&delete

--- src/main/java/com/AAA/controller/ProductController.java
+++ src/main/java/com/AAA/controller/ProductController.java
@@ -3,9 +3,11 @@ package com.AAA.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,4 +41,25 @@ public class ProductController {
 		Product product = productService.getProductById(productId);
 		return ResponseEntity.status(HttpStatus.CREATED).body(product);
 	} 
+	
+	@PutMapping("/products/{productId}")
+	public ResponseEntity<Product> updateProduct(@PathVariable Integer productId,@RequestBody @Valid ProductRequest productRequest){
+		//檢查product是否存在
+		Product product=productService.getProductById(productId);
+		if(product ==null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		
+		//修改商品的數據
+		productService.updateProduct(productId, productRequest);
+		Product updateProduct=productService.getProductById(productId);
+		return ResponseEntity.status(HttpStatus.OK).body(updateProduct);
+	}
+	
+	@DeleteMapping("/products/{productId}")
+	public ResponseEntity<?> deleteProduct(@PathVariable Integer productId){
+		productService.deleteProductById(productId);
+		
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
 }
