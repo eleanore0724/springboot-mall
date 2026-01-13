cc3d318533ee8307e5609a7d689027db357680ff
eleanore0724 <65076389+eleanore0724@users.noreply.github.com>
2026-01-14 02:03:53 +0800

    add limit&offset

--- src/main/java/com/AAA/controller/ProductController.java
+++ src/main/java/com/AAA/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +22,10 @@ import com.AAA.model.Product;
 import com.AAA.service.ProductService;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 
+@Validated
 @RestController
 public class ProductController {
 	
@@ -33,13 +37,18 @@ public class ProductController {
 			@RequestParam(required = false) ProductCategory category, 
 			@RequestParam(required = false) String search,
 			@RequestParam(defaultValue = "created_date") String orderBy,
-			@RequestParam(defaultValue = "desc") String sort
+			@RequestParam(defaultValue = "desc") String sort,
+			@RequestParam(defaultValue = "5") @Max(1000) @Min(0)Integer limit,
+			@RequestParam(defaultValue = "0") @Min(0) Integer offset
 	){
 		ProductQueryParams productQueryParams =new ProductQueryParams();
 		productQueryParams.setCategory(category);
 		productQueryParams.setSearch(search);
 		productQueryParams.setOrderBy(orderBy);
 		productQueryParams.setSort(sort);
+		productQueryParams.setLimit(limit);
+		productQueryParams.setOffset(offset);
+		
 		List<Product> productList = productService.getProducts(productQueryParams);
 		return ResponseEntity.status(HttpStatus.OK).body(productList);
 	}
